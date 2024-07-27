4ea0bd1b07d911cca941a5dcfe967dc830017cfd
Umang Chauhan <umang4191@outlook.com>
2024-07-27 20:50:45 +0200

    Added logic to create a product, skipping the db part for now

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,17 +1,30 @@
 ﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
 
 namespace Catalog.API.Products.CreateProduct;
 
 public record CreateProductCommand
-    (string Name, List<string> Category, string Description, string ImangeFile, decimal Price)
+    (string Name, List<string> Category, string Description, string ImageFile, decimal Price)
     : ICommand<CreateProductResult>;
 public record CreateProductResult(Guid Id);
 
 internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
-    public Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
-        // Business logic to create a product
-        throw new NotImplementedException();
+        // Create Product entity from command object
+        var product = new Product
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+
+        // save to database
+        
+        // return CreateProductResult result
+        return new CreateProductResult(Guid.NewGuid());
     }
 }
